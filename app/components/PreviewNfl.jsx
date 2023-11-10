9de58938fde0ff660bcf92b3af5928acaff1a10d
AntoineCalmettes <calmettes.antoine2@gmail.com>
2023-11-10 17:30:55 +0100

    add rules to eslintrc.sjon

--- app/components/PreviewNfl.jsx
+++ app/components/PreviewNfl.jsx
@@ -6,7 +6,7 @@ const PreviewNfl = () => {
     return (
         <Fragment>
             <div className=' flex justify-center pb-85'>
-                <h1 className='fs-64'>Your </h1><h1 className='ml-5 fs-64 gradient-text'>nft's</h1>
+                <h1 className='fs-64'>Your </h1><h1 className='ml-5 fs-64 gradient-text'>nft&apos;'s</h1>
             </div>
             <div className='flex justify-center'>
             <CardNft url={'metaverse.jpeg'} width={'264'} height={'352'} nameCard={'#Metaverse'} author={'By TheSalvare'}/>
--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-//  module.exports = nextConfig
- module.exports = {
-    basePath: '/landing-page-react', // Nom du référentiel GitHub
-    assetPrefix: '/landing-page-react/', // Nom du référentiel GitHub
-    // ... (autres configurations)
-  }
+ module.exports = nextConfig
